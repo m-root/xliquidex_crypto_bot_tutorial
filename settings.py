90abdb82a5d4e0c824bd5a2c3d58a2d5b2518a62
m-root <toyot@100>
2018-06-09 22:37:08 +0300

    Initial Settings configurations

---
+++ settings.py
@@ -0,0 +1,12 @@
+import gdax
+
+passphrase = ""
+key = ""
+b64secret = ""
+auth_client = gdax.AuthenticatedClient(key, b64secret, passphrase)
+maxTrades = 1
+open_trade_time_limit = 1
+minimum_order_fill_time = 1
+limit_entry_point_diff = 1
+
+
