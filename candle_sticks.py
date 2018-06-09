fa4e9602c9d540cc94c3ebee604f0cd890aa6af9
m-root <toyot@100>
2018-06-09 23:10:45 +0300

    Added volume and changed the database

--- candle_sticks.py
+++ candle_sticks.py
@@ -1,18 +1,13 @@
-from settings import product
+from settings import auth_client, product
 import time, sqlite3
 from datetime import datetime
-from ticker_price import Ticker
 
-endtime = time.time() + 60
-while time.time() < endtime:
-    value = datetime.utcnow().strftime("%S")
-    entTime = int(value)
-    print(60 - entTime)
+currentTime = datetime.utcnow().strftime("%Y-%m-%d %H:%M")
+while currentTime == datetime.utcnow().strftime("%Y-%m-%d %H:%M"):
+    print(60 - int(datetime.utcnow().strftime("%S")))
     time.sleep(1)
-    if entTime == 0 or entTime == 1:
-        break
 
-conn = sqlite3.connect('crypto_scalper.db')
+conn = sqlite3.connect('xliquidex.db')
 c = conn.cursor()
 stack = []
 
@@ -21,32 +16,26 @@ class Candle(object):
     def candleStick(self):
         while float(datetime.utcnow().strftime("%S.%f")) > 0:
             x = []
+            a = []
             init_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M")
-
             while init_time == datetime.utcnow().strftime("%Y-%m-%d %H:%M"):
-
-                ticker_price = Ticker()
-                ticker_price = ticker_price.ticker()
-
-                tickerPrice = float(ticker_price)
-                print('*****API*****candle.py - Product Ticker: ', tickerPrice)
-                x.append(tickerPrice)
+                y = float(auth_client.get_product_ticker(product)['price'])
+                x.append(y)
+                b = float(auth_client.get_product_ticker(product)['size'])
+                a.append(b)
                 time.sleep(1)
                 print(float(datetime.utcnow().strftime("%S.%f")))
-
             timex = float((datetime.utcnow() - datetime(1970,1,1)).total_seconds())
             c.execute(
                 "CREATE TABLE IF NOT EXISTS candleSticks (id integer primary key autoincrement, product Text, candle_time TEXT, candle_low REAL, candle_high REAL, candle_open REAL, candle_close REAL )"
             )
 
-            c.execute("INSERT INTO candleSticks ( candle_time, product, candle_low, candle_high, candle_open, candle_close) VALUES (?,?, ?, ?, ?, ?)",
-                      (timex, product,  min(x), max(x), x[0], x[-1]))
+            c.execute("INSERT INTO candleSticks ( candle_time, product, candle_low, candle_high, candle_open, candle_close, volume) VALUES (?,?, ?, ?, ?, ?, ?)",
+                      (timex, product,  min(x), max(x), x[0], x[-1], sum(a) ))
             conn.commit()
-
 
             c.execute('SELECT * FROM candleSticks WHERE ID = (SELECT MAX(ID) FROM candleSticks) ')
             data = c.fetchall()
             for data in data:
                 stack.append(list(data))
                 print(stack[-5:])
-
